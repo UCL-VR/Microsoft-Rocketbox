e6e7cadc5373fa8e7e0cb1601bd1a8973f213656
Sebastian Friston <sebastian.friston@gmail.com>
2024-08-23 13:01:50 +0100

    Updated three point rig to work with new version of ubiq

--- Assets/ThreePointStandaloneRig.cs
+++ Assets/ThreePointStandaloneRig.cs
@@ -2,57 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Ubiq.Avatars;
+using Ubiq.XRI;
+using Ubiq;
 
 public class ThreePointStandaloneRig : MonoBehaviour
 {
-    private Ubiq.Avatars.Avatar avatar;
-
-    private struct State
-    {
-        public Vector3 headPosition;
-        public Vector3 leftHandPosition;
-        public Vector3 rightHandPosition;
-        public Quaternion headRotation;
-        public Quaternion leftHandRotation;
-        public Quaternion rightHandRotation;
-    }
-
     public Transform left;
     public Transform right;
     public Transform head;
 
-    private State state;
+    private Ubiq.Avatars.AvatarManager avatarManager;
 
-    private void Awake()
-    {
-        avatar = GetComponent<Ubiq.Avatars.Avatar>();
-    }
-
-    // Start is called before the first frame update
     void Start()
     {
-        var avatarManager = FindAnyObjectByType<AvatarManager>();
-        avatar.SetHints(avatarManager.hints);
+        avatarManager = FindAnyObjectByType<AvatarManager>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (avatar.hints != null)
+        if(avatarManager.input.TryGet(out IHeadAndHandsInput src))
         {
-            avatar.hints.TryGetVector3("RightHandPosition", out state.rightHandPosition);
-            avatar.hints.TryGetVector3("LeftHandPosition", out state.leftHandPosition);
-            avatar.hints.TryGetVector3("HeadPosition", out state.headPosition);
-            avatar.hints.TryGetQuaternion("RightHandRotation", out state.rightHandRotation);
-            avatar.hints.TryGetQuaternion("LeftHandRotation", out state.leftHandRotation);
-            avatar.hints.TryGetQuaternion("HeadRotation", out state.headRotation);
+            left.position = src.leftHand.value.position;
+            left.rotation = src.leftHand.value.rotation;
+            right.position = src.rightHand.value.position;
+            right.rotation = src.rightHand.value.rotation;
+            head.position = src.head.value.position;
+            head.rotation = src.head.value.rotation;
         }
-
-        left.position = state.leftHandPosition;
-        left.rotation = state.leftHandRotation;
-        right.position = state.rightHandPosition;
-        right.rotation = state.rightHandRotation;
-        head.position = state.headPosition;
-        head.rotation = state.headRotation;
     }
 }
